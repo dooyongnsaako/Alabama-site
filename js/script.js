ec6cb45b9ad3ea9b458aa5dbd8b578c266b732d4
Nsaako Dooyong <dooyongnsaakobalderama@gmail.com>
2020-11-19 02:24:29 +0700

    added some features in like sliders, filter

--- js/script.js
+++ js/script.js
@@ -1,7 +1,16 @@
 // sidenav
-var menubtn = document.querySelector(".menuBar");
+var menubtn = document.querySelector(".menuBar"),
+    filterbtn = document.querySelector(".filter-btn");
 
-menubtn.addEventListener("click", function() {
-    alert("hello");
+filterbtn .addEventListener("click", function() {
+   let filterResult = document.querySelector(".filter-result");
+   filterResult.classList.toggle("show-filter-result");
+});
+
+$(document).ready(function($) {
+    $(".slider").slick({
+        slidesToShow: 4,
+        slidesToScroll: 4
+    });
 });
 
